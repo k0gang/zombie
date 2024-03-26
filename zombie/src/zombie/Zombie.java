7182ad595b56b39285c1f13622abdba61fd31a59
k0gang <k0gang@naver.com>
2024-03-26 14:15:07 +0900

    Add : Zombie

--- zombie/src/zombie/Zombie.java
+++ zombie/src/zombie/Zombie.java
@@ -1,5 +1,24 @@
 package zombie;
 
-public class Zombie {
-
+public class Zombie extends Unit {
+	private int power;
+	
+	public Zombie(int pos, int hp, int max) {
+		super(pos, hp, max);
+	}
+	
+	@Override
+	public void attack(Unit hero) {
+		power = r.nextInt(max)+1;
+		
+		hero.setHp(hero.getHp()-power);
+		
+		if(hero.getHp() <= 0)
+			hero.setHp(0);
+		
+		this.setHp(this.getHp()+power/2);
+		
+		System.out.printf("ZOMBIE가 %d의 피해를 입히고 %d만큼 회복\nHero hp : %d"
+				+ "\nZombie hp : %d",power,hero.getHp(),this.getHp());
+	}
 }
