bc5b07ab70fc2b9490ee7d832b809c5d7f73ba5d
k0gang <k0gang@naver.com>
2024-03-26 13:53:43 +0900

    Add : ZombieGame, Game

---
+++ zombie/src/zombie/Game.java
@@ -0,0 +1,18 @@
+package zombie;
+
+public class Game {
+	
+	private Game() {
+		
+	}
+	
+	private static Game instance = new Game();
+	
+	public static Game getInstance() {
+		return instance;
+	}
+	
+	public void run() {
+		
+	}
+}
