6995e21e1c4567037d1e1bd7452f9564fc1c6470
k0gang <k0gang@naver.com>
2024-03-26 15:12:51 +0900

    Add : Boss

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -1,5 +1,7 @@
 package zombie;
 
+import java.util.Random;
+
 //Boss 클래스는 Zombie 클래스를 상속받는다.
 //Boss 클래스는 보호막 shield 멤버변수를 가진다.
 //공격 메소드 void attack( Unit enemy ) 
@@ -7,6 +9,46 @@ package zombie;
 //필살기 : 25%의 확률로 1~max 사이의 공격력의 2배 만큼으로 상대를 공격하는 기능 (출력 예 : “보스가 30 의 공격력으로 필살기 공격 : 현재 Hero의 hp는 25”)
 //Boss 클래스는 생성자를 가짐(현재위치, 체력,공격력, 보호막)
 
-public class Boss {
-
+public class Boss extends Zombie implements enemy{
+	int shield;
+	
+	public void setShield(int shield) {
+		this.shield = shield;
+	}
+	
+	public int getShield() {
+		return this.shield;
+	}
+	
+	public Boss(int pos, int hp, int max, int shield) {
+		super(pos,hp,max);
+		this.shield = shield;
+	}
+	
+	@Override
+	public void attack(Unit hero) {
+		Random r = new Random();
+		int a = r.nextInt(4);
+		
+		if(a == 0) {
+			System.out.println("BOSS의 파워 스트라이크");
+			
+			int power = 2 * (r.nextInt(max)+1);
+			hero.setHp(hero.getHp()-power);
+			if(hero.getHp() <= 0)
+				hero.setHp(0);
+			
+			System.out.printf("BOSS가 %d의 피해를 입힘\nHero hp : %d",power,hero.getHp());
+			
+		}else {
+			System.out.println("BOSS의 공격");
+			int power = r.nextInt(max)+1;
+			hero.setHp(hero.getHp()-power);
+			if(hero.getHp() <= 0)
+				hero.setHp(0);
+			
+			System.out.printf("BOSS가 %d의 피해를 입힘\nHero hp : %d",power,hero.getHp());
+		}
+	}
+	
 }
